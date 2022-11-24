99ff77c6e94f22e5956b27fdd50f89f923d79c2e
Kseniia Des <kseniya250496@mail.ru>
2022-11-24 18:18:31 +0300

    add for for

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -1,69 +1,78 @@
-﻿// Вид 1
+﻿// // Вид 1
 
-void Method1()
-{
-    Console.WriteLine("Автор...");
-}
-// Method1();
-
-
-
-// Вид 2
-
-void Method2(string msg)
-{
-    Console.WriteLine(msg);
-}
-//Method2(msg: "Текст сообщения");
-
-void Method21(string msg, int count)
-{
-    int i = 0;
-    while (i < count)
-    {
-        Console.WriteLine(msg);
-        i++;
-    }
-}
-//Method21(msg: "Текст", count: 4);
-//Method21(count: 4, msg: "Новый текст");
+// void Method1()
+// {
+//     Console.WriteLine("Автор...");
+// }
+// // Method1();
 
 
-// Вид 3
 
-int Method3()
-{
-    return DateTime.Now.Year;
-}
-int year = Method3();
-//Console.WriteLine(year);
+// // Вид 2
+
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// //Method2(msg: "Текст сообщения");
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// //Method21(msg: "Текст", count: 4);
+// //Method21(count: 4, msg: "Новый текст");
 
 
-// Вид 4
+// // Вид 3
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3();
+// //Console.WriteLine(year);
+
+
+// // Вид 4
+
+// // string Method4(int count, string Text)
+// // {
+// //     int i = 0;
+// //     string result = String.Empty;
+
+// //     while (i < count)
+// //     {
+// //         result = result + Text;
+// //         i++;
+// //     }
+// //     return result;
+// // }
+
 
 // string Method4(int count, string Text)
 // {
-//     int i = 0;
 //     string result = String.Empty;
-
-//     while (i < count)
+//     for (int i = 0; i < count; i++)
 //     {
 //         result = result + Text;
-//         i++;
 //     }
 //     return result;
 // }
 
+// string res = Method4(5, "z");
+// Console.WriteLine(res);
 
-string Method4(int count, string Text)
+for (int i = 2; i <= 10; i++)
 {
-    string result = String.Empty;
-    for (int i = 0; i < count; i++)
+    for (int j = 2; j <= 10; j++)
     {
-        result = result + Text;
+        Console.WriteLine($"{i} x {j} = {i * j}");
     }
-    return result;
+    Console.WriteLine ();
 }
-
-string res = Method4(5, "z");
-Console.WriteLine(res);
